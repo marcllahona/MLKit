9d76183eb6eb29558288e6beeda62418cc01d1a6
Guled <somnibyte@gmail.com>
2016-07-06 00:33:47 -0500

    Beautified my swift code. :)

--- MLKit/DataManager.swift
+++ MLKit/DataManager.swift
@@ -10,125 +10,101 @@ import Foundation
 import Upsurge
 
 /**
- 
+
  The Numeric Type Protocol allows for generic functions
  to take in integers or doubles or floats.
- 
-*/
+
+ */
 public protocol NumericType {
-    func +(lhs: Self, rhs: Self) -> Self
-    func -(lhs: Self, rhs: Self) -> Self
-    func *(lhs: Self, rhs: Self) -> Self
-    func /(lhs: Self, rhs: Self) -> Self
-    func %(lhs: Self, rhs: Self) -> Self
+    func + (lhs: Self, rhs: Self) -> Self
+    func - (lhs: Self, rhs: Self) -> Self
+    func * (lhs: Self, rhs: Self) -> Self
+    func / (lhs: Self, rhs: Self) -> Self
+    func % (lhs: Self, rhs: Self) -> Self
     init(_ v: Int)
 }
 
+extension Double: NumericType { }
+extension Float: NumericType { }
+extension Int: NumericType { }
+extension Int8: NumericType { }
+extension Int16: NumericType { }
+extension Int32: NumericType { }
+extension Int64: NumericType { }
+extension UInt: NumericType { }
+extension UInt8: NumericType { }
+extension UInt16: NumericType { }
+extension UInt32: NumericType { }
+extension UInt64: NumericType { }
 
-extension Double : NumericType { }
-extension Float  : NumericType { }
-extension Int    : NumericType { }
-extension Int8   : NumericType { }
-extension Int16  : NumericType { }
-extension Int32  : NumericType { }
-extension Int64  : NumericType { }
-extension UInt   : NumericType { }
-extension UInt8  : NumericType { }
-extension UInt16 : NumericType { }
-extension UInt32 : NumericType { }
-extension UInt64 : NumericType { }
+public class MLDataManager {
 
-
-public class MLDataManager{
-
-    
     enum MLDataHandelingError: ErrorType {
-        
+
     }
-    
-    
+
     /**
      The sumUpData method takes in array of numbers (of any type using the NumericType protocol) and computes the sum of all the numbers in the array.
-     
+
      - parameter data: An array of numbers.
-     
+
      - returns: The sum of the array
      */
     public static func sumUpData<T: NumericType> (data: Array<T>) -> T {
         var sum = T(0)
-        for val  in data{
+        for val in data {
             sum = sum + val
         }
         return sum
     }
-    
+
     /**
      The mean method calculates the mean of an array of numbers (of any type using the NumericType protocol).
-     
+
      - parameter data: An array of numbers.
-     
+
      - returns: The mean of the array
      */
     public static func mean<T: NumericType> (data: Array<T>) -> T {
         let totalSum = sumUpData(data)
         let totalAmountOfData = T(data.count)
-        return totalSum/totalAmountOfData
+        return totalSum / totalAmountOfData
     }
-    
-    
-    
+
     /**
      The dataToMatrix method takes an array of features (which contain your data of a specific feature), along with your observations/output
-     and turns your features into a Matrix of type Float and your output into an array in order to be processed by machine learning algorithms 
+     and turns your features into a Matrix of type Float and your output into an array in order to be processed by machine learning algorithms
      such as calculating the RSS/cost function for Regression.
-     
-     - parameter features: An array of your features (which are suppose to be arrays as well). 
+
+     - parameter features: An array of your features (which are suppose to be arrays as well).
      - parameter output: An array of your observations/output.
-     
+
      - returns: A tuple that consists of a matrix and a array of type ValueArray
      */
-    public static func dataToMatrix (features:[Array<Float>], output: Array<Float>) -> (Matrix<Float>,ValueArray<Float>) {
-        
-        
-        // Create Output Matrix 
-        let output_matrix = Matrix<Float>(rows:output.count, columns: 1, elements:output)
-        
-        // Create "contant/intercept" list 
+    public static func dataToMatrix (features: [Array<Float>], output: Array<Float>) -> (Matrix<Float>, ValueArray<Float>) {
+
+        // Create Output Matrix
+        let output_matrix = Matrix<Float>(rows: output.count, columns: 1, elements: output)
+
+        // Create "contant/intercept" list
         let contant_array = [Float](count: features[0].count, repeatedValue: 1.0)
-        var matrix_as_array:[[Float]] = []
-        
+        var matrix_as_array: [[Float]] = []
+
         for (i, _) in contant_array.enumerate() {
-            var new_row:[Float] = []
+            var new_row: [Float] = []
             new_row.append(contant_array[i])
-            
+
             for feature_array in features {
                 new_row.append(feature_array[i])
             }
-            
+
             matrix_as_array.append(new_row)
         }
-        
+
         let feature_matrix = Matrix<Float>(matrix_as_array)
-        
+
         return (feature_matrix, output_matrix.elements)
     }
-    
 
-    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
--- MLKit/SimpleLinearRegression.swift
+++ MLKit/SimpleLinearRegression.swift
@@ -8,46 +8,42 @@
 
 import Foundation
 
+public class SimpleLinearRegression {
 
-public class SimpleLinearRegression{
-    
-    
-    public var slope:Float!
-    public var intercept:Float!
-    public var cost_function_result:Float!
-    
-    public init(){
+    public var slope: Float!
+    public var intercept: Float!
+    public var cost_function_result: Float!
+
+    public init() {
         slope = 0.0
         intercept = 0.0
         cost_function_result = 0.0
     }
-    
-    
+
     /**
      The fitUsingGradientDescent method fits your model (that consists of one feature and one output array) and returns your regression coefficients/weights. The methods applies gradient descent
      as a means to find the most optimal regression coefficients for your model.
-     
+
      - parameter inputFeature: An array of your feature. Only 1 feature is allowed. This class is used for simple experiments in which only 1 feature is allowed.
      - parameter output: An array of your observations/output.
      - parameter step_size: The amount of "steps" you want to take in the gradient descent process.
      - parameter tolerance: The stopping point. Since it might take awhile to hit 0 you can set a tolerance to stop at a specific point.
-     
+
      - returns: A tuple of your slope and intercept (your regression coefficients).
      */
-    public func fitUsingGradientDescent (inputFeature:Array<Float>, output:Array<Float>, step_size: Float, tolerance: Float) -> (Float, Float){
-        
-        
-        if(inputFeature.count == 0 || output.count == 0){
+    public func fitUsingGradientDescent (inputFeature: Array<Float>, output: Array<Float>, step_size: Float, tolerance: Float) -> (Float, Float) {
+
+        if (inputFeature.count == 0 || output.count == 0) {
             print("You need to have 1 feature array and 1 output array to utilize this function.")
-            return (Float(-1),Float(-1))
+            return (Float(-1), Float(-1))
         }
-        
-        if(inputFeature.count != output.count){
-            
+
+        if (inputFeature.count != output.count) {
+
             print("The length of your input feature data and your output must be the same in order to utilize this function.")
             return (Float(-1), Float(-1))
         }
-        
+
         var converged = false // Convergence Boolean
         var current_slope = Float(0.0) // Slope / Weight
         var current_intercept = Float(0.0)
@@ -58,172 +54,167 @@ public class SimpleLinearRegression{
         var error_and_input_sum = Float(0.0)
         var adjustment_for_slope = Float(0.0)
         var gradient_magnitude = Float(0.0)
-        
+
         // Function to subtract predictions from the actual observations
-        let subtract_predictions_from_output = { (predictions:Array<Float>, output:Array<Float>) -> Array<Float> in
+        let subtract_predictions_from_output = { (predictions: Array<Float>, output: Array<Float>) -> Array<Float> in
             var new_data = Array<Float>()
-            
-            for(i, data) in predictions.enumerate(){
+
+            for (i, data) in predictions.enumerate() {
                 let value = data - output[i]
                 new_data.append(value)
             }
-            
+
             return new_data
         }
-        
+
         // Function to find the product of our errors and input feature data
         let find_product_of_error_and_input = { () -> Array<Float> in
-            
+
             var new_data = Array<Float>()
-            
-            for(i, data) in errors.enumerate(){
+
+            for (i, data) in errors.enumerate() {
                 let value = data * inputFeature[i]
                 new_data.append(value)
             }
-            
+
             return new_data
         }
-    
+
         while !converged {
-            
+
             // Compute the predicted values given the current slope and intercept
             predictions_from_training_data = getTrainingDataPredictions(inputFeature, intercept: current_intercept, slope: current_slope)
-            
+
             // Compute the prediction errors (prediction - Y)
             errors = subtract_predictions_from_output(predictions_from_training_data, output)
-            
+
             // Update the intercept
             sum_of_errors = MLDataManager.sumUpData(errors)
             adjustment_for_intercept = step_size * sum_of_errors
             current_intercept = current_intercept - adjustment_for_intercept
-            
+
             // Update the slope
             let error_input_arr = find_product_of_error_and_input()
             error_and_input_sum = MLDataManager.sumUpData(error_input_arr)
             adjustment_for_slope = step_size * error_and_input_sum
             current_slope = current_slope - adjustment_for_slope
-            
+
             // Compute the magnitude of the gradient
             let sum_of_errors_squared = (sum_of_errors * sum_of_errors)
             let error_and_input_sum_squared = error_and_input_sum * error_and_input_sum
             gradient_magnitude = sqrt(sum_of_errors_squared + error_and_input_sum_squared)
-        
+
             // Check for convergence
             if gradient_magnitude < tolerance {
-               converged = true
+                converged = true
             }
 
         }
-        
-        
+
         self.slope = current_slope
         self.intercept = current_intercept
-        
+
         return (current_slope, current_intercept)
     }
-    
-    
+
     /**
      The fitUsingNoGradientDescent method fits your model by taking the derivative of the residual sum of squares formula (An .md file with the intuition behind this approach will be provided soon) and solves for your regression coefficients.
-     
+
      - parameter inputFeature: An array of your feature. Only 1 feature is allowed. This class is used for simple experiments in which only 1 feature is allowed.
      - parameter output: An array of your observations/output.
 
      - returns: A tuple of your slope and intercept (your regression coefficients).
      */
-    public func fitUsingNoGradientDescent (inputFeature:Array<Float>, output:Array<Float> ) -> (Float, Float){
-        
-        
-        if(inputFeature.count == 0 || output.count == 0){
+    public func fitUsingNoGradientDescent (inputFeature: Array<Float>, output: Array<Float>) -> (Float, Float) {
+
+        if (inputFeature.count == 0 || output.count == 0) {
             print("You need to have 1 feature array and 1 output array to utilize this function.")
-            return (Float(-1),Float(-1))
+            return (Float(-1), Float(-1))
         }
-        
-        if(inputFeature.count != output.count){
-            
+
+        if (inputFeature.count != output.count) {
+
             print("The length of your input feature data and your output must be the same in order to utilize this function.")
             return (Float(-1), Float(-1))
         }
-        
+
         // The sum over all of our observations/output (y)
         let y_sub_i = MLDataManager.sumUpData(output)
-        
+
         // The sum over all of our input data (x)
         let x_sub_i = MLDataManager.sumUpData(inputFeature)
-        
+
         // The sum over all of our input data squared
-        let data_squared = inputFeature.map { $0 * $0}
+        let data_squared = inputFeature.map { $0 * $0 }
         let x_sub_i_squared = MLDataManager.sumUpData(data_squared)
-        
+
         // The sum over all of our input data and output data
-        var input_and_output_multiplied:Array<Float> = []
+        var input_and_output_multiplied: Array<Float> = []
         for (i, element) in inputFeature.enumerate() {
             let value = element * output[i]
             input_and_output_multiplied.append(value)
         }
         let x_sub_i_y_sub_i = MLDataManager.sumUpData(input_and_output_multiplied)
-        
+
         // Calculate the slope (w1)
-        let numerator =  x_sub_i_y_sub_i - (Float(1.0) / Float(inputFeature.count)) * (y_sub_i * x_sub_i)
-        let denominator = x_sub_i_squared  - (Float(1.0) / Float(inputFeature.count)) * (x_sub_i * x_sub_i)
-        let slope = numerator/denominator
-        
-        // Calculate the intercept 
+        let numerator = x_sub_i_y_sub_i - (Float(1.0) / Float(inputFeature.count)) * (y_sub_i * x_sub_i)
+        let denominator = x_sub_i_squared - (Float(1.0) / Float(inputFeature.count)) * (x_sub_i * x_sub_i)
+        let slope = numerator / denominator
+
+        // Calculate the intercept
         let intercept = MLDataManager.mean(output) - slope * MLDataManager.mean(inputFeature)
-        
-        
-        // Save the current slope and intercept 
+
+        // Save the current slope and intercept
         self.slope = slope
         self.intercept = intercept
-        
+
         return (slope, intercept)
     }
-    
-    
+
     /**
      The RSS method computes the residual sum of squares or the cost function of your model.
-     
+
      - parameter inputFeature: An array of your feature. Only 1 feature is allowed. This class is used for simple experiments in which only 1 feature is allowed.
      - parameter output: An array of your observations/output.
      - parameter intercept: Your intercept weight (of type Float).
      - parameter slope: your slope weight (of type Float).
-     
+
      - returns: The cost of your model (a.k.a The Residual Sum of Squares).
-     
+
      */
-    public func RSS (inputFeature: Array<Float>, output:Array<Float>, intercept:Float, slope: Float) -> Float {
+    public func RSS (inputFeature: Array<Float>, output: Array<Float>, intercept: Float, slope: Float) -> Float {
         var sum = Float(0.0)
         for (i, _) in inputFeature.enumerate() {
-           let value = (output[i] - predict(intercept, slope: slope, inputValue: inputFeature[i]))
-           let value_squared = value * value
-           sum = sum + value_squared
+            let value = (output[i] - predict(intercept, slope: slope, inputValue: inputFeature[i]))
+            let value_squared = value * value
+            sum = sum + value_squared
         }
-    
-        // Save RSS/Cost 
+
+        // Save RSS/Cost
         self.cost_function_result = sum
-        
+
         return sum
     }
-    
+
     /**
-     The predict method is used for making one-time predictions by passing your intercept weight, slope weight, and an input value. 
-     
+     The predict method is used for making one-time predictions by passing your intercept weight, slope weight, and an input value.
+
      - parameter intercept: Your intercept weight (of type Float).
      - parameter slope: your slope weight (of type Float).
-     - paramter inputValue: An input value. 
-     
+     - paramter inputValue: An input value.
+
      - returns: A prediction (of type Float).
      */
     public func predict (intercept: Float, slope: Float, inputValue: Float) -> Float {
         let y_hat = intercept + slope * inputValue
         return y_hat
     }
-    
+
     func getTrainingDataPredictions (inputFeature: Array<Float>, intercept: Float, slope: Float) -> Array<Float> {
-        
+
         var predictions_from_training_data = Array<Float>()
-        
-        for data in inputFeature{
+
+        for data in inputFeature {
             let yhat = predict(intercept, slope: slope, inputValue: data)
             predictions_from_training_data.append(yhat)
         }
@@ -231,24 +222,4 @@ public class SimpleLinearRegression{
         return predictions_from_training_data
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
